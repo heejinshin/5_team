281b4a04adf5cfa0604c86974428479bd4cce5b1
heejinshin <heejinshin02@gmail.com>
2023-05-22 00:39:55 +0900

    service app setting

--- map_system/service/urls.py
+++ map_system/service/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from . import views
+
+app_name = 'service'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("index", views.index),
+    path("map", views.show_map),
     
 ]
--- map_system/service/views.py
+++ map_system/service/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'choosebar.html')
+
+def show_map(request):
+    return render(request, 'map.html')
+
